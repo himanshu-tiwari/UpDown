f7277c66c20132a11f953df4d959c42bc56172d8
Himanshu Tiwari <himanshutiwari@Himanshus-MacBook-Air.local>
2018-09-10 21:28:35 +0530

    Added URL parsing

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const http = require('http');
+const url = require('url');
 
 var server = http.createServer(function(req, res) {
+	var parsedUrl = url.parse(req.url, true);
+
+	var path = parsedUrl.pathname;
+	var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+	console.log('Request received on path: ' + trimmedPath);
+
 	res.end('Hello World!\n');
 });
 
