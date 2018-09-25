4445d4c324079f2f4044b47a007b2da9a67ba58d
Himanshu Tiwari <himanshutiwari@Himanshus-MacBook-Air.local>
2018-09-25 21:46:52 +0530

    Added request routing to the project

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
+const config = require('./config');
 
 var server = http.createServer(function(req, res) {
 	var parsedUrl = url.parse(req.url, true);
@@ -21,14 +22,49 @@ var server = http.createServer(function(req, res) {
 	req.on('end', function(){
 		buffer += decoder.end();
 
-		console.log('Request received on path: ' + trimmedPath + ' with method: ' + method + ' and with these query string parameters', queryStringObject);
-		console.log('Request received with these headers: ', headers);
-		console.log('Request received with these payload: ', buffer);
+		var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : router['notFound'];
+		var data = {
+			'trimmedPath' : trimmedPath,
+			'queryStringObject' :  queryStringObject,
+			'method' : method,
+			'headers' : headers,
+			'payload' : buffer
+		};
 
-		res.end('Hello World!\n');
+		chosenHandler(data, function(statusCode, payload) {
+			statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+			payload = typeof(payload) == 'object' ? payload : {};
+
+			var payloadString = JSON.stringify(payload);
+
+			res.setHeader('Content-Type', 'application/json');
+			res.writeHead(statusCode);
+			res.end(payloadString);
+
+			console.log('Response: ', statusCode, payloadString);
+		});
+
+		// console.log('Request received on path: ' + trimmedPath + ' with method: ' + method + ' and with these query string parameters', queryStringObject);
+		// console.log('Request received with these headers: ', headers);
+		// console.log('Request received with these payload: ', buffer);
 	});
 });
 
-server.listen(3000, function() {
-	console.log("This server is listening to port 3000 now.");
+server.listen(config.port, function() {
+	console.log("This server is listening on port "+config.port+" in "+config.envName+" mode.");
 });
+
+var handlers = {};
+
+handlers.sample = function(data, callback) {
+	callback(406, {'name' : 'sample handler'});
+};
+
+handlers.notFound = function(data, callback) {
+	callback(404);
+};
+
+var router = {
+	'sample' : handlers.sample,
+	'notFound' : handlers.notFound,
+};
